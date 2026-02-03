d4199e5fefc74207c6fbe95ac70569dfaaf45e13
Idowu Adeniran (DFS) <Idowu.Adeniran@dfs.ny.gov>
2026-02-03 17:40:47 -0500

    Initial Angular project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `<h1>Hello DevOps Team!
+  </h1>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
